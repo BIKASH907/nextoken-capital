184567683c70617e1d806bf10b19eb65ab07f405
Bikash Bhat <YOUR_GITHUB_EMAIL>
2026-03-18 19:32:10 +0200

    add global navbar for all pages

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      padding: "30px",
+      background: "#0b0f14",
+      borderTop: "1px solid #1f2937",
+      textAlign: "center",
+      color: "#aaa"
+    }}>
+      <p>© 2026 Nextoken Capital. All rights reserved.</p>
+    </footer>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import '../styles/globals.css'
-import { AuthProvider } from '../lib/AuthContext'
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
+    <>
+      <Navbar />
       <Component {...pageProps} />
-    </AuthProvider>
-  )
+      <Footer />
+    </>
+  );
 }
