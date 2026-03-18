b80650d0ff6a7b87f63aa6e541978e90fdfe72d5
Bikash Bhat <YOUR_GITHUB_EMAIL>
2026-03-18 18:33:26 +0200

    add markets page

---
+++ markets.js
@@ -0,0 +1,8 @@
+export default function Markets() {
+  return (
+    <div style={{ padding: "40px", color: "white", background: "black", minHeight: "100vh" }}>
+      <h1>Markets</h1>
+      <p>This is the markets page.</p>
+    </div>
+  );
+}
