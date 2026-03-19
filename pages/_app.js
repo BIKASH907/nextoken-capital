8dcebc7dffec3d87580a99e8ed621567038ff5f1
Bikash Bhat <YOUR_GITHUB_EMAIL>
2026-03-20 01:43:55 +0200

    fix: implement global navbar and ensure default exports

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/globals.css';
+import Navbar from '../components/Navbar'; // Import the new Navbar
 import Footer from '../components/Footer';
 
-// This MUST be "export default" to reveal your pages
 export default function MyApp({ Component, pageProps }) {
   return (
     <div style={{ backgroundColor: "#05060a", minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+      <Navbar /> {/* This makes it appear on every page */}
       <main style={{ flex: 1 }}>
         <Component {...pageProps} />
       </main>
