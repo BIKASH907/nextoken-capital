c4c9f3fbae517f7cd4da9ae7a7944002a69ee613
Bikash Bhat <YOUR_GITHUB_EMAIL>
2026-03-19 20:27:01 +0200

    fix: absolute path to relative for globals.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import Header from "../components/Header";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {/* This is your Binance-style Header */}
       <Header />
-      <div className="pt-24"> 
+      
+      {/* The pt-24 (96px) ensures your page content 
+        starts below the fixed header so nothing is hidden.
+      */}
+      <main className="pt-24 min-h-screen bg-[#0b0e11] text-white">
         <Component {...pageProps} />
-      </div>
+      </main>
     </>
   );
 }
