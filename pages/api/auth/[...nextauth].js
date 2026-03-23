bf84ada7667ecc3a4f82bc1f3a7115be92dbacc6
Bikash Bhat <YOUR_GITHUB_EMAIL>
2026-03-23 19:49:55 +0200

    fix: disable PKCE for Google OAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -37,6 +37,7 @@ export const authOptions = {
       clientId:     process.env.GOOGLE_CLIENT_ID     || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
       authorization: { params: { prompt: 'consent', access_type: 'offline', response_type: 'code' } },
+      checks: ['state'],
     }),
   ],
   callbacks: {
