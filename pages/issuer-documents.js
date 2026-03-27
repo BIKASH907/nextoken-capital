5d743566197141a91d1fd6cf304c72c36041ffb7
Bikash Bhat <YOUR_GITHUB_EMAIL>
2026-03-27 02:14:28 +0200

    fix: issuer-documents getServerSideProps

--- pages/issuer-documents.js
+++ pages/issuer-documents.js
@@ -208,3 +208,5 @@ export default function IssuerDocuments() {
   );
 }
 export const getServerSideProps = async () => ({ props: {} });
+
+export const getServerSideProps = async () => ({ props: {} });
