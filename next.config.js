b7f23c4d70f2b484da372e27619cd1af49da50e0
Bikash Bhat <YOUR_GITHUB_EMAIL>
2026-04-02 00:08:57 +0300

    fix: CSP in next.config allows Google Translate

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
         { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
         { key: 'X-XSS-Protection', value: '1; mode=block' },
         { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
-        { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com https://js.stripe.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://res.cloudinary.com https://*.cloudinary.com; connect-src 'self' https://api.monerium.dev https://api.monerium.app https://api.resend.com https://js.stripe.com https://*.sumsub.com; frame-src https://js.stripe.com https://*.sumsub.com;" },
+        { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com https://js.stripe.com https://translate.google.com https://translate.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://translate.google.com https://www.google.com data: blob: https://res.cloudinary.com https://*.cloudinary.com; connect-src 'self' https://translate.googleapis.com https://api.monerium.dev https://api.monerium.app https://api.resend.com https://js.stripe.com https://*.sumsub.com; frame-src https://js.stripe.com https://*.sumsub.com;" },
         { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
       ]
     }]
